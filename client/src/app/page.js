11c55ff9dec18c61816fb4d3f71716d998b5107e
MahmmadhusenDodiya <rajudodiya072@gmail.com>
2024-05-04 12:19:29 +0530

    Added OAuth Code Partial completed

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Room from "./pages/room";
 import UploadForm from "./components/uploadForm";
+import AuthPage from "./pages/auth";
 
 
 
 export default function Home() {
   return (
     <div>
-    <UploadForm/>
+    {/* <UploadForm/> */}
     {/* <Room/> */}
+    <AuthPage/>
     </div>
   );
 }
--- upload_service/controllers/upload.controller.js
+++ upload_service/controllers/upload.controller.js
@@ -1,44 +1,37 @@
 import fs from 'fs'
+import path from 'path';
+const __dirname = path.resolve();
 
+export const uploadToAWSLocal = async (req, res) => {
 
-export const uploadToAWSLocal=async(req,res)=>{
-
-  //  const filepath = 'C:\\Users\\DELL\\Desktop\\Youtube-hld\\upload_service\\ss.png';
   console.log("Upload file is there :) ");
-  if(!req.file){
+  if (!req.file) {
     console.log("Upload File not Received");
     return res.status(400).send('no File Received');
   }
 
 
-    const file=req.file;
-    const binaryDataBuffer = Buffer.from(file);
+  const file = req.file;
+  console.log(__dirname);
+  let filePath = path.resolve(__dirname, '..','AwsLocal');
 
-    const filePath = '../../AwsLocal';
+  console.log('Grandparent directory:', filePath);
 
+  filePath=path.join(filePath, file.originalname);
+  console.log("File With Path="+filePath);
 
-// Write content to the file
-fs.writeFile(filePath, binaryDataBuffer, (err) => {
-  if (err) {
-    console.error('Error writing file:', err);
-  } else {
-    console.log('File saved successfully!');
-  }
-});
-
-
-  // //Check if file exist
-  // if(!fs.existsSync(filepath)){
-  //   console.log('File does not exist : ',filepath);
-  //   res.status(500).send({"message":"file Not Found Error"});
-  //   return;
-  // }
-  // else {
-  //   console.log("File Found");
-  // }
+  fs.writeFile(filePath, file.buffer, (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('File saved successfully!');
+    }
+  });
 
 
-  res.status(200).send({"message":"Response found 1234"})
+
+
+  res.status(200).send({ "message": "Response found 1234" })
 
 
 
