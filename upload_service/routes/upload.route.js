54d48bb357ea8d845bf7bdbb175e37dae5f36fbc
MahmmadhusenDodiya <rajudodiya072@gmail.com>
2024-05-07 16:28:27 +0530

    Multipart Upload Completed

--- upload_service/routes/upload.route.js
+++ upload_service/routes/upload.route.js
@@ -1,6 +1,7 @@
 import express from "express"
 import uploadFileToS3, { uploadToAWSLocal } from "../controllers/upload.controller.js";
 import multer from 'multer'
+import multipartUploadFileToS3 from "../controllers/multipartupload.controller.js";
 const upload = multer();
 
 const router=express.Router();
@@ -8,10 +9,15 @@ const router=express.Router();
 // router.post('/',upload.single('file'),uploadToAWSLocal);
 
 
+// Upload Local Router
 //router.post('/', upload.fields([{ name: 'chunk' }, { name: 'totalChunks' }, { name: 'chunkIndex' }]), uploadToAWSLocal);
 
-router.post('/', upload.fields([{ name: 'chunk' }, { name: 'totalChunks' }, { name: 'chunkIndex' }]), uploadFileToS3);
+// Upload S3 Router
+// router.post('/', upload.fields([{ name: 'chunk' }, { name: 'totalChunks' }, { name: 'chunkIndex' }]), uploadFileToS3);
 
+
+//Upload direct from backed to S3 Entire in One request
+// router.post('/', multipartUploadFileToS3);
 
 router.get('/',(req,res)=>{
 
